d15320aa3b12ee0bb80c25642bf89d4dc7804ab3
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-04 11:47:17 +0300

    Dbcontext доавлен в CustomerRepository

--- src/Otus.Teaching.Concurrency.Import.DataAccess/Repositories/CustomerRepository.cs
+++ src/Otus.Teaching.Concurrency.Import.DataAccess/Repositories/CustomerRepository.cs
@@ -1,15 +1,23 @@
 using System;
+using Otus.Teaching.Concurrency.Import.DataAccess.EF;
 using Otus.Teaching.Concurrency.Import.Handler.Entities;
 using Otus.Teaching.Concurrency.Import.Handler.Repositories;
 
 namespace Otus.Teaching.Concurrency.Import.DataAccess.Repositories
 {
-    public class CustomerRepository
-        : ICustomerRepository
+    public class CustomerRepository : ICustomerRepository
     {
+        private DatabaseContext _dataContext;
+
+        public CustomerRepository(DatabaseContext dbContext)
+        {
+            _dataContext = dbContext;
+        }
+
         public void AddCustomer(Customer customer)
         {
-            //Add customer to data source   
+            //Add customer to data source
+            _dataContext.Customers.Add(customer);
         }
     }
 }
