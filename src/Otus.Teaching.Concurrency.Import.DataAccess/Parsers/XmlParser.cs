9b9bdf37017e833594f68205df97e3f4906fdfff
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-04 12:20:02 +0300

    десериализация Xml

--- src/Otus.Teaching.Concurrency.Import.DataAccess/Parsers/XmlParser.cs
+++ src/Otus.Teaching.Concurrency.Import.DataAccess/Parsers/XmlParser.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using Otus.Teaching.Concurrency.Import.Core.Parsers;
+using Otus.Teaching.Concurrency.Import.DataGenerator.Dto;
 using Otus.Teaching.Concurrency.Import.Handler.Entities;
 
 namespace Otus.Teaching.Concurrency.Import.DataAccess.Parsers
 {
-    public class XmlParser
-        : IDataParser<List<Customer>>
+    public class XmlParser : IDataParser<List<Customer>>
     {
+        string _path;
+
+        public XmlParser(string filePath)
+        {
+            _path = filePath;
+        }
+
         public List<Customer> Parse()
         {
             //Parse data
-            return new List<Customer>();
+            using var reader = new FileStream(_path, FileMode.Open);
+
+            var deserializer = new XmlSerializer(typeof(CustomersList));
+            var customersList = (CustomersList)deserializer.Deserialize(reader);
+            return customersList.Customers;
+
         }
     }
 }
