097161f7d07f212c0064cccd9e7ee1b4dd9cce8f
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-04 11:46:23 +0300

    SQLite DBcontext

---
+++ src/Otus.Teaching.Concurrency.Import.DataAccess.EF/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Otus.Teaching.Concurrency.Import.Handler.Entities;
+
+namespace Otus.Teaching.Concurrency.Import.DataAccess.EF
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Customers.db");
+        }
+    }
+}
