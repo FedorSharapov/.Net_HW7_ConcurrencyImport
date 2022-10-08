2982b4390ed88988a76aed83cb4f076035f81341
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:24:03 +0300

    добавлен csv тип для фабрики

--- src/Otus.Teaching.Concurrency.Import.DataAccess/ParserFactory.cs
+++ src/Otus.Teaching.Concurrency.Import.DataAccess/ParserFactory.cs
@@ -1,15 +1,21 @@
 using Otus.Teaching.Concurrency.Import.Core.Parsers;
 using Otus.Teaching.Concurrency.Import.DataAccess.Parsers;
 using Otus.Teaching.Concurrency.Import.Handler.Entities;
+using Otus.Teaching.Concurrency.Import.Common;
 using System.Collections.Generic;
 
 namespace Otus.Teaching.Concurrency.Import.DataAccess
 {
     public static class ParserFactory
     {
-        public static IDataParser<IEnumerable<Customer>> GetParser(string filePath)
+        public static IDataParser<IEnumerable<Customer>> GetParser(string provider, string filePath)
         {
-            return new XmlParser(filePath);
+            if (provider.Equals(TypesFiles.Xml))
+                return new XmlParser(filePath);
+            else if (provider.Equals(TypesFiles.Csv))
+                return new CsvParser(filePath);
+
+            throw new System.Exception($"Invalid type parser \"{provider}\".");
         }
     }
 }
