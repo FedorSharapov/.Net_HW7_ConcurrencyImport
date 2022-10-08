e54fef2e781ad4290c78b4d796fcafd3d32c8080
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:22:39 +0300

    Типы файлов

---
+++ src/Common/TypesFiles.cs
@@ -0,0 +1,8 @@
+﻿namespace Otus.Teaching.Concurrency.Import.Common
+{
+    public static class TypesFiles
+    {
+        public static readonly string Xml = "xml";
+        public static readonly string Csv = "csv"; 
+    }
+}
