22eb0cd5f686c749bb91e76a4b091b27ff11bcc4
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:23:35 +0300

    добавлен CsvParser

---
+++ src/Otus.Teaching.Concurrency.Import.DataAccess/Parsers/CsvParser.cs
@@ -0,0 +1,25 @@
+﻿using Otus.Teaching.Concurrency.Import.Core.Parsers;
+using Otus.Teaching.Concurrency.Import.Handler.Entities;
+using ServiceStack.Text;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Otus.Teaching.Concurrency.Import.DataAccess.Parsers
+{
+    public class CsvParser : IDataParser<IEnumerable<Customer>>
+    {
+        string _path;
+
+        public CsvParser(string filePath)
+        {
+            _path = filePath;
+        }
+
+        public IEnumerable<Customer> Parse()
+        {
+            using var fileStream = new FileStream(_path, FileMode.Open);
+            var customers = CsvSerializer.DeserializeFromStream<IEnumerable<Customer>>(fileStream);
+            return customers;
+        }
+    }
+}
