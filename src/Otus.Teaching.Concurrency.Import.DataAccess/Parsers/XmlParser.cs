c00a6ff65d7aead207e4c6200c2a67ef1edc37fa
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:23:15 +0300

    рефакторинг XmlParser

--- src/Otus.Teaching.Concurrency.Import.DataAccess/Parsers/XmlParser.cs
+++ src/Otus.Teaching.Concurrency.Import.DataAccess/Parsers/XmlParser.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Xml.Serialization;
 using Otus.Teaching.Concurrency.Import.Core.Parsers;
-using Otus.Teaching.Concurrency.Import.DataGenerator.Dto;
 using Otus.Teaching.Concurrency.Import.Handler.Entities;
 
 namespace Otus.Teaching.Concurrency.Import.DataAccess.Parsers
@@ -19,9 +18,9 @@ namespace Otus.Teaching.Concurrency.Import.DataAccess.Parsers
         public IEnumerable<Customer> Parse()
         {
             using var fileStream = new FileStream(_path, FileMode.Open);
-            var xmlSerializer = new XmlSerializer(typeof(CustomersList));
-            var customersList = xmlSerializer.Deserialize(fileStream) as CustomersList;
-            return customersList.Customers;
+            var xmlSerializer = new XmlSerializer(typeof(IEnumerable<Customer>));
+            var customers = xmlSerializer.Deserialize(fileStream) as IEnumerable<Customer>;
+            return customers;
         }
     }
 }
