1242a4d90014448ae38aa10f41543909fec9cb7d
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:49:34 +0300

    добавлено расширение для получения части коллекции

---
+++ src/Common/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Otus.Teaching.Concurrency.Import.Common
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> GetPart<T>(this IEnumerable<T> source, int skip, int take)
+        {
+            return source.Skip(skip).Take(take);
+        }
+    }
+}
