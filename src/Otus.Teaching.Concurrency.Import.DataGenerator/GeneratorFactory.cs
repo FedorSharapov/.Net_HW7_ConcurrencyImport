626cd93634d8cc273a72ea94ffc75cca902d00e8
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-08 18:25:29 +0300

    добавлен csv для фабрики генераторов

--- src/Otus.Teaching.Concurrency.Import.DataGenerator/GeneratorFactory.cs
+++ src/Otus.Teaching.Concurrency.Import.DataGenerator/GeneratorFactory.cs
@@ -1,3 +1,4 @@
+using Otus.Teaching.Concurrency.Import.Common;
 using Otus.Teaching.Concurrency.Import.DataGenerator.Generators;
 using Otus.Teaching.Concurrency.Import.Handler.Data;
 
@@ -5,9 +6,14 @@ namespace Otus.Teaching.Concurrency.Import.DataGenerator
 {
     public static class GeneratorFactory
     {
-        public static IDataGenerator GetGenerator(string fileName, int dataCount)
+        public static IDataGenerator GetGenerator(string provider, string fileName, int dataCount)
         {
-            return new XmlGenerator(fileName, dataCount);
+            if (provider.Equals(TypesFiles.Xml))
+                return new XmlGenerator(fileName, dataCount);
+            else if (provider.Equals(TypesFiles.Csv))
+                return new CsvGenerator(fileName, dataCount);
+
+            throw new System.Exception($"Invalid type generator \"{provider}\".");
         }
     }
 }
